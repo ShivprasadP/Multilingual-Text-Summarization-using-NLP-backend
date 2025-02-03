7165b21aaca0499054ea09155097401a26e95fc2
ShivprasadP <vihs7777@gmail.com>
2025-02-03 16:16:52 +0530

    PDF printing format changed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const summariesRouter = require('./routes/Summaries');
-const usersRouter = require('./routes/Users');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const summariesRouter = require("./routes/Summaries");
+const usersRouter = require("./routes/Users");
 
 dotenv.config();
 
@@ -13,17 +13,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB Atlas');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB Atlas:', error);
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
 
-app.use('/summaries', summariesRouter);
-app.use('/users', usersRouter);
+app.use("/summaries", summariesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
